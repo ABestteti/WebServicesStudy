a5c830f4db4cf0d1e2dc2788298cbeab11d6cb72
anderson.bestteti@gmail.com <anderson.bestteti@gmail.com>
2015-05-25 15:57:03 -0300

    Added ch1 project from book

--- WebServiceStudy/src/com/acaosistemas/jwsur2/ch1/UrlConnectionClient.java
+++ WebServiceStudy/src/com/acaosistemas/jwsur2/ch1/UrlConnectionClient.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-import javax.management.RuntimeErrorException;
-
 public class UrlConnectionClient {
 
 	public static void main(String[] args) {
